61680be4adcb822413dcfb7baef9d41d8d52d32f
Samuel Pereira <samuel.p.araujo@gmail.com>
2018-05-19 23:50:17 -0300

    Simple refactor

--- src/main/java/org/fileinterpreter/parser/LineParser.java
+++ src/main/java/org/fileinterpreter/parser/LineParser.java
@@ -2,7 +2,7 @@ package org.fileinterpreter.parser;
 
 import org.fileinterpreter.exception.MisconfiguredDocumentException;
 
-public abstract class LineParser {
-    public abstract void parse(String content, Object object) throws MisconfiguredDocumentException;
-    public abstract String toContent(Object object) throws MisconfiguredDocumentException;
+public interface LineParser {
+    void parse(String content, Object object) throws MisconfiguredDocumentException;
+    String toContent(Object object) throws MisconfiguredDocumentException;
 }
